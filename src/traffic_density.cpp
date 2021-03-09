e907e86f912cffb62b87f03c9fb1e1bdc842de05
VNSAditya02 <adityabommakanti2002@gmail.com>
2021-03-09 19:05:01 +0530

    feat: Updated code

--- src/traffic_density.cpp
+++ src/traffic_density.cpp
@@ -23,23 +23,11 @@ namespace po = boost::program_options;
 
 int QueueDensity(const string& location, int nth_frame);
 Mat CropImage(Mat frame);
-void findDensity(vector<int> y, string filename);
+void writeInFile(vector<double> y, string filename);
 int DynamicDensity(const string& location, int nth_frame);
+double count (Mat frame, int rows, int cols, int threshold);
 int writeOut();
 
-static void on_low_H_thresh_trackbar(int, void *);
-static void on_high_H_thresh_trackbar(int, void *);
-static void on_low_S_thresh_trackbar(int, void *);
-static void on_high_S_thresh_trackbar(int, void *);
-static void on_low_V_thresh_trackbar(int, void *);
-static void on_high_V_thresh_trackbar(int, void *);
-
-// Used to find optimum values of inRange() function, line: 161
-const int MAX_VALUE_H = 255;
-const int MAX_VALUE = 255;
-const int VIDEO_FRAME_RATE = 15;
-const String WINDOW_CAPTURE_NAME = "Video Capture";
-const String WINDOW_DETECTION_NAME = "Object Detection";
 const String OUT_PATH = "out";
 const char pathSeparator =
 #ifdef _WIN32
@@ -47,9 +35,6 @@ const char pathSeparator =
 #else
         '/';
 #endif
-
-int low_H = 0, low_S = 0, low_V = 0;
-int high_H = MAX_VALUE_H, high_S = MAX_VALUE, high_V = MAX_VALUE;
 
 /**
  * Location of the input video file has to be given by command line argument
@@ -144,15 +129,11 @@ int DynamicDensity(const string& location, int nth_frame){
         return -1;
     }
 
-    // Create Background subtractor object
-    Ptr<BackgroundSubtractor> backSub;
-    backSub = createBackgroundSubtractorMOG2();
-
     // Initialize
     int i = 0;
 
     // frame_1 stores the previous frame
-    Mat frame, frame_1;
+    Mat frame;
 
     // read the previous frame
     capture >> frame;
@@ -160,103 +141,55 @@ int DynamicDensity(const string& location, int nth_frame){
     // Crop the previous frame
     frame = CropImage(frame);
 
-    // Subtract the background
-    backSub -> apply(frame, frame_1);
-
     // Stores the output values
-    vector <int> y;
+    vector <double> y;
 
     while(true)
     {
         // frame_2 is the current frame
-        // frame_threshold is the output frame which detects moving objects
-        Mat frame2, frame_2, frame_threshold, out, magnitude, angle, magn_norm;
-
-        vector<vector<Point> > contours;
-        vector<Vec4i> hierarchy;
+        // out is the output frame which detects moving objects
+        Mat frame2, out;
 
         // Change the condition accordingly, i % 1 process every frame, i % 3 process every third frame
         if (i % nth_frame == 0){
+
             // Read the current frame
             capture >> frame2;
+
             // If frame is empty, exit the loop
             if (frame2.empty()){
                 break;
             }
+
             // Crop the present frame
             frame2 = CropImage(frame2);
 
-            // Uncomment the following lines to change the values in Line: 161; inRange() function
+            // Finds the difference between the current and previous frames
+            absdiff(frame2, frame, out);
 
-            /*
-            namedWindow(WINDOW_CAPTURE_NAME);
-            namedWindow(WINDOW_DETECTION_NAME);
-            createTrackbar("Low H", WINDOW_DETECTION_NAME, &low_H, MAX_VALUE_H, on_low_H_thresh_trackbar);
-            createTrackbar("High H", WINDOW_DETECTION_NAME, &high_H, MAX_VALUE_H, on_high_H_thresh_trackbar);
-            createTrackbar("Low S", WINDOW_DETECTION_NAME, &low_S, MAX_VALUE, on_low_S_thresh_trackbar);
-            createTrackbar("High S", WINDOW_DETECTION_NAME, &high_S, MAX_VALUE, on_high_S_thresh_trackbar);
-            createTrackbar("Low V", WINDOW_DETECTION_NAME, &low_V, MAX_VALUE, on_low_V_thresh_trackbar);
-            createTrackbar("High V", WINDOW_DETECTION_NAME, &high_V, MAX_VALUE, on_high_V_thresh_trackbar);
-            */
+            // rows denotes no of rows in output frame, col denotes no of columns in output frame
+            int rows = out.rows;
+            int cols = out.cols;
 
-            // flow stores
-            Mat optflow(frame_1.size(), CV_32FC2);
+            // Counts no of pixels whose pixel value is greater than 10
+            double count_pix = count(out, rows, cols, 10);
 
-            // Apply Background subtractor for current frame
-            backSub -> apply(frame2, frame_2);
+            // Add the output to the output vector
+            y.push_back(count_pix);
 
-            // Computes the optical flow between frames
-            calcOpticalFlowFarneback(frame_1, frame_2, optflow, 0.5, 3, 15, 3, 5, 1.2, 0);
-
-            Mat flow_parts[2];
-
-            // Splits optflow into two parts
-            split(optflow, flow_parts);
-
-            // Finds cartersian co-ordinates between two matrices
-            cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
-
-            // Normalizes the magnitude
-            normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
-
-            angle *= ((1.f / 360.f) * (180.f / 255.f));
-
-            //builds hsv image
-            Mat hsv[3], hsv_img, hsv_final, color_img;
-            hsv[0] = angle;
-            hsv[1] = Mat::ones(angle.size(), CV_32F);
-            hsv[2] = magn_norm;
-            merge(hsv, 3, hsv_img);
-            hsv_img.convertTo(hsv_final, CV_8U, 255.0);
-
-            // Convert hsv image to color image
-            cvtColor(hsv_final, color_img, COLOR_HSV2BGR);
-
-            // Convert color image to grayscale
-            cvtColor(color_img, out, COLOR_BGR2GRAY);
-
-            // Detects pixels whose pixel values lies in the given range
-            // These values are found using trackbars. I found these values as optimum for finding moving  objects
-            inRange(out, Scalar(40,45,50), Scalar(125,149,180), frame_threshold);
-
-            // Detect edges of moving objects
-            Canny( frame_threshold, out, 100, 200);
-
-            // Finds contours of the detected edges
-            findContours(out.clone(), contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-
-            // Add contours size to output vector
-            y.push_back(contours.size());
-
-            // Current slides becomes next slide for the next iteration
-            frame_1 = frame_2;
+            // Changes current frame to previous frame
+            frame = frame2;
         }
-        else
-            capture >> frame_2;
+
+        else{
+            capture >> frame2;
+        }
+
         i++;
     }
     // Find density of the output value
-    findDensity(y, OUT_PATH + pathSeparator + "DynamicDensity.txt");
+    writeInFile(y, OUT_PATH + pathSeparator + "DynamicDensity.txt");
+
     return 0;
 }
 
@@ -267,6 +200,12 @@ int DynamicDensity(const string& location, int nth_frame){
   * @return
   */
 int QueueDensity(const string& location, int nth_frame){
+
+    // Opens the empty frame
+    Mat src_colour = imread("../COP290/assets/empty_final.jpg");
+
+    // Finds the cropped image of the input image
+    Mat src_crop = CropImage(src_colour);
 
     // Opens the video file
     VideoCapture capture(location);
@@ -283,16 +222,11 @@ int QueueDensity(const string& location, int nth_frame){
 
     Mat frame, fgMask;
 
-    // threshold value used in edge detector
-    int thresh = 100;
-
     // Stores the value of no of contours in a frame
-    std::vector<int> y;
+    std::vector<double> y;
 
     while (true)
     {
-        vector<vector<Point> > contours;
-        vector<Vec4i> hierarchy;
         // Change the condition accordingly, i % 1 process every frame, i % 3 process every third frame
         if (i % nth_frame == 0)
         {
@@ -306,15 +240,19 @@ int QueueDensity(const string& location, int nth_frame){
             // Finds the cropped image of the given frame
             frame = CropImage(frame);
 
-            // Detects edges and applies Mask
-            Canny( frame, fgMask, thresh, thresh*2 );
+            // Finds the difference between the present frame and empty frame
+            absdiff(frame, src_crop, fgMask);
 
-            // Find contours
-            findContours(fgMask.clone(), contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+            // rows denotes no of rows in output frame, col denotes no of columns in output frame
+            int row = fgMask.rows;
+            int col = fgMask.cols;
+
+            // Counts no of pixels whose pixel value is greater than 15
+            double x =count(fgMask, row, col, 15);
+
+            // Add the output to the output vector 
+            y.push_back(x);
             
-            // Add the contours size to y
-            y.push_back(contours.size());
-            // cout << contours.size() << endl;
         }
         else
             capture >> frame;
@@ -323,9 +261,28 @@ int QueueDensity(const string& location, int nth_frame){
     }
 
     // Calls Density function
-     findDensity(y, OUT_PATH + pathSeparator + "QueueDensity.txt");
+     writeInFile(y, OUT_PATH + pathSeparator + "QueueDensity.txt");
 
     return 0;
+}
+
+/**
+ * Given a frame, this frame finds the no of pixels whose pixel value is greater than a threshold
+ * @param frame
+ * @param row: no of rows in the frame
+ * @param cols: no of columns in the frame
+ */
+double count (Mat frame, int rows, int cols, int threshold){
+
+    int count = 0;
+    for (int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            if(frame.at<uchar>(j, i) > threshold){
+                count++;
+            }
+        }
+    }
+    return (double)count/double(rows*cols);
 }
 
 /**
@@ -371,23 +328,14 @@ Mat CropImage(Mat frame){
  * @param y : integer Vector
  * @param filename : name of the file in which output is to be stored
  */
-void findDensity(vector<int> y, string filename){
-    // Stores the maximum value of the vector
-    int max_val = *max_element(y.begin(), y.end());
-
-    // Stores the minimum value of the vector
-    int min_val = *min_element(y.begin(), y.end());
-
-    double z;
-    int range = max_val - min_val;
+void writeInFile(vector<double> y, string filename){
 
     // Creates file and stores output value in out.txt
     ofstream MyFile(filename);
 
-    // Traverse the vector and find the normalized values
-    for (int it: y) {
-        z = (double)(it - min_val)/(double)range;
-        MyFile << z << endl;
+    // Traverse the vector and adds the values to the output file
+    for (double it: y) {
+        MyFile << it << endl;
     }
 
     // Closes the output file
@@ -429,42 +377,4 @@ int writeOut() {
     queue.close();
     dynamic.close();
     return 0;
-}
-
-/*
-
-NOTE : I copy pasted these functions from a website, just to find optimum values for inRange() function
-       Should remove these functions before submitting
-
-*/
-
-static void on_low_H_thresh_trackbar(int, void *)
-{
-    low_H = min(high_H-1, low_H);
-    setTrackbarPos("Low H", WINDOW_DETECTION_NAME, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-    high_H = max(high_H, low_H+1);
-    setTrackbarPos("High H", WINDOW_DETECTION_NAME, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-    low_S = min(high_S-1, low_S);
-    setTrackbarPos("Low S", WINDOW_DETECTION_NAME, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-    high_S = max(high_S, low_S+1);
-    setTrackbarPos("High S", WINDOW_DETECTION_NAME, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-    low_V = min(high_V-1, low_V);
-    setTrackbarPos("Low V", WINDOW_DETECTION_NAME, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-    high_V = max(high_V, low_V+1);
-    setTrackbarPos("High V", WINDOW_DETECTION_NAME, high_V);
 }
